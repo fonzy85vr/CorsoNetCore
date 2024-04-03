01433844e9fc58dc4abd7947b45444954a823cca
marco.andreoli <marco.andreoli@eng.it>
2024-04-03 14:34:32 +0200

    Add project files.

---
+++ CorsoNetCore/Controllers/CoursesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CorsoNetCore.Controllers {
+    public class CoursesController : Controller {
+        public IActionResult Index() {
+            return Content("Home corsi");
+        }
+
+        public IActionResult Detail(string id) {
+            return Content($"Dettaglio corso {id}");
+        }
+    }
+}
---
+++ CorsoNetCore/Program.cs
@@ -0,0 +1,21 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddMvc();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment()) {
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Courses}/{action=Index}/{id?}");
+
+app.Run();
