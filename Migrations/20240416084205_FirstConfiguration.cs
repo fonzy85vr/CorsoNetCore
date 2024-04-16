a1fc6fbe3db931fcaa9efbf8167a5b402162b602
marco.andreoli <marco.andreoli@eng.it>
2024-04-16 10:47:02 +0200

    aggiunta gestione migrazioni

---
+++ Migrations/20240416084205_FirstConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CorsoNetCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirstConfiguration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Courses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    ImagePath = table.Column<string>(type: "TEXT", nullable: false),
+                    Author = table.Column<string>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false),
+                    Rating = table.Column<double>(type: "REAL", nullable: false),
+                    CurrentPrice_Currency = table.Column<string>(type: "TEXT", nullable: false),
+                    CurrentPrice_Amount = table.Column<decimal>(type: "TEXT", nullable: false),
+                    FullPrice_Currency = table.Column<string>(type: "TEXT", nullable: false),
+                    FullPrice_Amount = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Courses", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Courses");
+        }
+    }
+}
